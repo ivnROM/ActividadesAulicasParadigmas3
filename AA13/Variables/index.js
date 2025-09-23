897dd2438d91109346fa36326541d4bf86afb750
ivan meyer <meyerivanoliver@gmail.com>
2025-09-23 19:48:12 -0300

    avances aa13

---
+++ AA13/Variables/index.js
@@ -0,0 +1,9 @@
+let nombreEmpleado = "Marta Martita";
+let sueldo = 45000;
+
+console.log("Nombre del empleado:");
+console.log(nombreEmpleado);
+console.log("");
+console.log("Sueldo:");
+console.log(sueldo);
+
