ff174ba2dc247e37bdb8fd271464620a3f679a28
ivan meyer <meyerivanoliver@gmail.com>
2025-10-02 16:04:07 -0300

    refactorizacion

--- AA13/crear_ejercicios.sh
+++ AA13/crear_ejercicios.sh
@@ -2,8 +2,24 @@
 
 n_ejercicios=$1
 path=$2
+content="
+<!DOCTYPE html>
+<html>
+    <head>
+      <title></title>
+    </head>
+    <body>
+      <h1>Ejercicio : </h1>
+
+      <script>
+      </script>
+    </body>
+  </html>
+"
 
 for ((i=1; i<=n_ejercicios; i++))
 do
-  touch "$path/ejercicio$i.html"
+  ejercicio="$path/ejercicio$i.html"
+  touch "$ejercicio"
+  echo "$content" > "$ejercicio"
 done
